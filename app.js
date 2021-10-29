cf0f4907f0e6b5a30f78d8857728242be0c8a8ee
Jiyong Sim <sjy19910222@gmail.com>
2021-10-29 23:47:59 +0900

    console.log(age);

--- app.js
+++ app.js
@@ -175,3 +175,9 @@ const calculaterSecond = {
 
 const plusResult = calculaterSecond.plus(10,2);
 const minusResult = calculaterSecond.minus(plusResult,3);
+
+//#2.13 Conditionals
+
+const age = parseInt(prompt("how old are you"));
+
+console.log(age);
