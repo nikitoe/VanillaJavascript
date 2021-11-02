52065ef2124b301e58feccfe70c2cc2369a5f58e
Jiyong Sim <sjy19910222@gmail.com>
2021-11-02 23:59:50 +0900

    Update app2.js

--- app2.js
+++ app2.js
@@ -11,4 +11,21 @@ function handleTitleClick () {
     title.style.color = "blue";
 }
 
+function handleMouseEnter () {
+    title.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave () {
+    title.innerText = "Mouse is gone!";
+}
+
 title.addEventListener("click", handleTitleClick);    //click에대한 event를 listen하겠다.
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
+
+//#3.4 Events part Two
+/**
+ * MDN(Mozailla Developer Network)을 통해서 HTML에대한 정보(event등)를 얻을수 있다.
+ * web APl가 포함된 페이지로 들어간다.
+ * 
+ */
