94699e507e62f99f0f5b8c410995f24433568a5e
Jiyong Sim <sjy19910222@gmail.com>
2021-11-03 11:37:48 +0900

    #3.5 More Events

--- app2.js
+++ app2.js
@@ -6,28 +6,52 @@
 //#3.4 Events part Two
 /**
  * MDN(Mozailla Developer Network)을 통해서 HTML에대한 정보(event등)를 얻을수 있다.
- * web APl가 포함된 페이지로 들어간다.
+ * h1 html element mdn을 검색 ->  Web APIs라는 문장이 포함된 페이지(JS관점의 HTML Heading Element란 의미)
  * 
  */
-const title = document.querySelector("div.hello:first-child h1");
+//#3.5 More Events
+/**
+ * 
+ */
+const h1 = document.querySelector("div.hello:first-child h1");
 
-console.dir(title);
+console.dir(h1);
 
 function handleTitleClick () {
-    title.style.color = "blue";
+    h1.style.color = "blue";
 }
 
 function handleMouseEnter () { 
-    title.innerText = "Mouse is here!";
-    title.style.color = "green";
+    h1.innerText = "Mouse is here!";
 }
 
 function handleMouseLeave () {
-    title.innerText = "Mouse is gone!";
-    title.style.color = "orange";
+    h1.innerText = "Mouse is gone!";
 }
 
-title.addEventListener("click", handleTitleClick);          //click에대한 event를 listen하겠다
-title.addEventListener("mouseenter", handleMouseEnter);     //mouseenter에대한 event를 listen하겠다
-title.addEventListener("mouseleave", handleMouseLeave);     //mouseleave에대한 event를 listen하겠다
+function handleWindowResize () {
+    document.body.style.backgroundColor = "tomato";     //title, head, body는 중요해서 document로 가져올 수 있다.
+}
 
+function handleWindowCopy () {
+    alert("copier!");
+}
+
+function handleWindowOffline () {
+    alert("SOS no WIFI");
+}
+
+function handleWindowOnline () {
+    alert("GOOD!");
+}
+
+h1.addEventListener("click", handleTitleClick);          //click에대한 event를 listen하겠다
+//h1.onclick = handleTitleClick;                         //위와 같은 의미이지만, addEventListener를 선호하는 이유는 removeEventListener를 통해서 event listener를 제거할 수 있기 때문이다.
+
+h1.addEventListener("mouseenter", handleMouseEnter);     //mouseenter에대한 event를 listen하겠다
+h1.addEventListener("mouseleave", handleMouseLeave);     //mouseleave에대한 event를 listen하겠다
+
+window.addEventListener("resize",handleWindowResize);    
+window.addEventListener("copy", handleWindowCopy);       
+window.addEventListener("offline", handleWindowOffline); //WIFI가 꺼져있는지 켜져있는지 알 수 있다.
+window.addEventListener("online", handleWindowOnline);   //
