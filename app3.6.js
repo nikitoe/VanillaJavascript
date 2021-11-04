d87c0f1db5358e6768d1ac83e4836604b20e0da2
Jiyong Sim <sjy19910222@gmail.com>
2021-11-04 14:48:57 +0900

    #3.8 CSS in Javascript part Three

--- app3.6.js
+++ app3.6.js
@@ -1,4 +1,4 @@
-//#3.6 CSS in Javascript
+//#3.6~8 CSS in Javascript
 /**
  * step 1 element를 찾아라
  * step 2 event를 listen해라
@@ -8,6 +8,7 @@ const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick () {
 
+    //#CSS in Javascript part One
     /*const currentColor = h1.style.color;
     let newColor;
 
@@ -19,12 +20,27 @@ function handleTitleClick () {
     h1.style.color = newColor;
     */
    //h1.className = "active";         //index.html의 h1태그의 클래스이름을 active로 지정한다.
-   const clickedClass = "clicked";
+
+   //#CSS in Javascript part Two
+   /*const clickedClass = "clicked";
    if (h1.className === clickedClass) {
        h1.className = "";
    } else {
        h1.className = clickedClass;
    }
+   */
+
+   //#3.8 CSS in Javascript part Three
+   /* const clickedClass = "clicked";
+    if (h1.classList.contains(clickedClass)) {
+        h1.classList.remove(clickedClass);
+    } else {
+       h1.classList.add(clickedClass);
+    }
+    */
+
+    h1.classList.toggle("clicked")                   //toggle은 h1의 classList에 clickedClass가 이미 있는지확인해서
+
 }
 
 h1.addEventListener("click", handleTitleClick);
