823fd4209414c3e1b72bac792f743a8453da65f8
Jiyong Sim <sjy19910222@gmail.com>
2021-11-04 13:12:22 +0900

    #3.6 CSS in Javascript

--- app3.6.js
+++ app3.6.js
@@ -1,3 +1,9 @@
+//#3.6 CSS in Javascript
+/**
+ * step 1 element를 찾아라
+ * step 2 event를 listen해라
+ * step 3 그event에 반응해라
+ */
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick () {
@@ -10,6 +16,7 @@ function handleTitleClick () {
     } else {
         newColor = "blue";
     }
+    h1.style.color = newColor;
 }
 
 h1.addEventListener("click", handleTitleClick);
