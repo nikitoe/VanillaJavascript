a94bb0c5fa84130cef22809b01b26b15fb74f8cd
Jiyong Sim <sjy19910222@gmail.com>
2021-11-07 15:46:20 +0900

    #4.2 Events

---
+++ app4.2.js
@@ -0,0 +1,11 @@
+//#4.2 Events
+
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+
+function onLoginSubmit (event) {
+    event.preventDefault();                                            //preventDefault란 어떤 event의 기본 행동(브라우저가 기본적으로 하는 function)이든지 발생되지 않도록 막는 것.
+    console.log(loginInput.value);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit)                     //submit이라는 event가 발생하면 onLoginSubmit function을 실행시킨다.
