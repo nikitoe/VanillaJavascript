9b11cb2a33fabd34494032e41904f79279c2436d
Jiyong Sim <sjy19910222@gmail.com>
2021-11-08 16:28:10 +0900

    #5.2 PadStart

--- js/clock.js
+++ js/clock.js
@@ -1,11 +1,28 @@
 //#5.0 Intervals
+/**
+ * setInterval는 몇초간격으로 계속 function을 실행시킨다.
+ */
+
 //#5.1 Timeouts and Dates
+/**
+ * new Date()오브젝트를 통해서 시간,분,초를 출력할 수 있다.
+ */
+
+//#5.2 PadStart 
+/**
+ * "1".padStart(2,"0")는 스트링'1'에서 2자리가 아닌경우 스트링'0'을 추가한다
+ * 
+ */
 
 const clock = document.querySelector("h2#clock");
 
 function getClock () {
     const date = new Date();
-    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 getClock();
