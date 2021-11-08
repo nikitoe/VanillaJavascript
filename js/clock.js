94954711b64b65c4643c2cb147f31bd8d25907b0
Jiyong Sim <sjy19910222@gmail.com>
2021-11-08 16:02:55 +0900

    #5.1 Timeouts and Dates

--- js/clock.js
+++ js/clock.js
@@ -1,10 +1,13 @@
 //#5.0 Intervals
+//#5.1 Timeouts and Dates
 
 const clock = document.querySelector("h2#clock");
 
-function sayHello () {
-    console.log("hello");
+function getClock () {
+    const date = new Date();
+    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
 
-setInterval(sayHello, 5000);                        //몇초간격으로 계속 function을 실행시킨다.
-
+getClock();
+setInterval(getClock, 1000);                        //몇초간격으로 계속 function을 실행시킨다.
+//setTimeout(sayHello, 5000);
