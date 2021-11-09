598a6d22abd6372774a339dba99e128ad5541352
Jiyong Sim <sjy19910222@gmail.com>
2021-11-10 00:32:15 +0900

    //#7.0 Setup

--- js/background.js
+++ js/background.js
@@ -4,7 +4,7 @@ const images = ["0.jpeg","1.jpeg","2.jpeg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
+const bgImage = document.createElement("img");                  //javascript에서 html을 만들어줌
 
 bgImage.src = `img/${chosenImage}`;
  
---
+++ js/todo.js
@@ -0,0 +1,13 @@
+//#7.0 Setup
+
+const toDoForm = document.querySelector("#todo-form");
+const toDoInput = document.querySelector("#todo-form input");
+const toDoList = document.querySelector("#todo-list");
+
+function handleToDoSubmit (event) {
+    event.preventDefault();
+    const newTodo = toDoInput.value;
+    toDoInput.value = "";
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
