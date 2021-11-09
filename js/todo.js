f6e07c2d7ea342cd26c39594e0dc4e4444b349ed
Jiyong Sim <sjy19910222@gmail.com>
2021-11-10 00:45:34 +0900

    #7.1 Adding ToDos

--- js/todo.js
+++ js/todo.js
@@ -1,13 +1,23 @@
 //#7.0 Setup
+//#7.1 Adding ToDos
 
 const toDoForm = document.querySelector("#todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.querySelector("#todo-list");
 
+function paintToDo (newTodo) {
+    const li = document.createElement("li");        //javascript에서 html태그 li를 생성
+    const span = document.createElement("span");    //javascript에서 html태그 span를 생성
+    li.appendChild(span);                           //변수 li에 변수 span을 추가
+    span.innerText = newTodo;                       //변수 span newTodo항목을 추가
+    toDoList.appendChild(li);                       //변수 toDoList에 변수 li를 추가
+}
+
 function handleToDoSubmit (event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
