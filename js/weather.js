c8878c1fefeffffb298f85a7f3c73c9ba71f27ea
Jiyong Sim <sjy19910222@gmail.com>
2021-11-10 19:48:52 +0900

    #8.1 Weather API

--- js/weather.js
+++ js/weather.js
@@ -1,12 +1,27 @@
 //#8.0 Geolocation 
+/**
+ * navigator.geolocation.getCurrentPosition(sucessful, error);
+ */
+//#8.1 Weather API
+/**
+ * JSON Viewer 구글 확장 프로그램 
+ */
+
+const API_KEY = "0f3ead24f6965057a53e805bd8f42ec1";
 
 function onGeoOk (position) {
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("You live in", lat, lng);
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    fetch(url).then(response => response.json()).then(data => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });             //javascript가 대신 url을 부른다.
 }
 
-function onGeoError () {
+function onGeoError () { 
     console.log("Can't find you. No weather for you.");
 }
 
