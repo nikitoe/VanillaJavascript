a340b22e4405cbf552644f26b172863197f44e3b
Jiyong Sim <sjy19910222@gmail.com>
2021-11-11 01:20:32 +0900

    update img

--- js/background.js
+++ js/background.js
@@ -1,11 +1,15 @@
 //#6.1 Background
 
-const images = ["0.jpeg","1.jpeg","2.jpeg"];
+const images = ["0.jpeg","1.jpeg","2.jpeg","3.jpeg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");                  //javascript에서 html을 만들어줌
 
-bgImage.src = `img/${chosenImage}`;
+const body = document.querySelector("body");
+
+body.style.backgroundImage = `linear-gradient( rgba(0,0,0, 0.5), rgba(0,0,0, 0.5) ), url('img/${chosenImage}')`;
+
+//bgImage.src = `img/${chosenImage}`;
  
-document.body.appendChild(bgImage);                               //appendChild()는 body에 html을 추가함
+//document.body.appendChild(bgImage);                               //appendChild()는 body에 html을 추가함
