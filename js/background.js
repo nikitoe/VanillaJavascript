e1ec9b253dac70a2004d2a2051b1573014dbf2a2
Jiyong Sim <sjy19910222@gmail.com>
2021-11-15 17:43:25 +0900

    update

--- js/background.js
+++ js/background.js
@@ -1,6 +1,6 @@
 //#6.1 Background
 
-const images = ["0.jpeg","1.jpeg","2.jpeg","3.jpeg"];
+const images = ["0.jpeg","1.jpeg","2.jpeg","3.jpeg"w];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
