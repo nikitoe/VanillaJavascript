ac6c890e9e91e21061eec90148696b32e147aa98
Jiyong Sim <sjy19910222@gmail.com>
2021-11-29 21:30:06 +0900

    #fixed error

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ function onGeoOk (position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
-    fetch(url).then(response => response.json()).then(                  => {
+    fetch(url).then(response => response.json()).then((data)=> {
         const weather = document.querySelector("#weather span:first-child");
         const city = document.querySelector("#weather span:last-child");
         city.innerText = data.name;
